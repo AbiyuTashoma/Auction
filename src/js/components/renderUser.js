230a45424399b096ea9fed8de50fad41bee67a46
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-12-27 21:14:34 +0100

    improved render feature

--- src/js/components/renderUser.js
+++ src/js/components/renderUser.js
@@ -1,20 +1,9 @@
-export function renderUser(
-  status,
-  userPfl,
-  usrName,
-  cdtContainer,
-  lgnBtn,
-  lgtBtn,
-) {
+export function renderUser(status, userPfl, usrName, cdtContainer) {
   if (status) {
     usrName.innerHTML = `${userPfl["name"]}  <img src="${userPfl["avatar"]}" class="avatar-small" alt="avatar"/>`;
     cdtContainer.innerHTML = userPfl["credits"];
-    lgnBtn.style.display = "none";
-    lgtBtn.style.display = "block";
   } else {
     usrName.innerHTML = "Menu";
     cdtContainer.innerHTML = 0;
-    lgnBtn.style.display = "block";
-    lgtBtn.style.display = "none";
   }
 }
