c0e58b2e7428ac042ac60ebcca13821600cab404
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-12-28 21:13:54 +0100

    working on updating avatar

--- src/js/components/renderProfile.js
+++ src/js/components/renderProfile.js
@@ -1,10 +1,30 @@
 export function viewProfile(prfl, prflContainer) {
   prflContainer.innerHTML = `<div class="avatar-container border position-relative mx-auto">
                                 <img src="${prfl["avatar"]}" class="avatar-bigger mx-auto" alt="avatar"/>
-                                <a class="custom-btn btn-primary position-absolute top-100 start-100 translate-middle badge rounded-pill">
+                                <a class="custom-btn btn-primary position-absolute top-100 start-100 translate-middle badge rounded-pill" data-bs-toggle="modal" data-bs-target="#modal-edit">
                                   edit
                                 </a>
                              </div>
-            <p class="my-3">${prfl["name"]}</p>
-            <p class="my-3">${prfl["credits"]}</p>`;
+                            
+                          <p class="my-3">${prfl["name"]}</p>
+                          <p class="my-3">${prfl["credits"]}</p>
+
+                           <div class="modal fade" id="modal-edit" tabindex="-1" aria-labelledby="editmodal" aria-hidden="true">
+                              <div class="modal-dialog">
+                                  <div class="modal-content">
+                                      <div class="modal-body p-3">
+                                          <div class="note-update"></div>             
+                                          <form class="update-form">
+                                              <div class="input-group my-2">
+                                                  <input type="url" id="edit-avatar" class="form-control" aria-label="edit avatar url" value='${prfl["avatar"]}'>
+                                              </div>
+                                              <div class="d-flex justify-content-center gap-2 mt-2">
+                                                  <input type="submit" id="update-btn" class="custom-btn btn-primary" value="Update avatar">
+                                                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" aria-label="Close">Close</button>
+                                              </div>
+                                          </form>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>`;
 }
--- src/js/profile.js
+++ src/js/profile.js
@@ -2,4 +2,9 @@ import { currentUser } from "./components/profileData.js";
 import { profileInfoContainer } from "./components/variables.js";
 import { viewProfile } from "./components/renderProfile.js";
 
+import { updateAvatarContainer } from "./components/variables.js";
+import { updateAvatar } from "./components/updateAvatar.js";
+
 viewProfile(currentUser, profileInfoContainer);
+
+updateAvatarContainer.addEventListener("submit", updateAvatar);
