572a5cc91f558240d87b58bd6f89b83083081b5c
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-12-31 12:12:10 +0100

    added listings feed to index html

--- src/js/components/feedHtml.js
+++ src/js/components/feedHtml.js
@@ -1,30 +1,28 @@
-export const html = `<div class="col">
+export async function createFeedHtml(rspns) {
+  let html = "";
+  let i = 0;
+  rspns.forEach((element) => {
+    const innerCarousel = createCarousel(element["media"]);
+
+    html += `<div class="col">
             <div class="card h-100">
-                <div id="carousel-1" class="carousel slide" data-bs-ride="carousel">
+                <div id="carousel-${i}" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="src/temp/car/joshua-koblin-eqW1MPinEV4-unsplash.jpg" class="card-img-top d-block w-100" alt="..."/>
-                        </div>
-                        <div class="carousel-item"> 
-                            <img src="src/temp/car/martin-katler-WBsl72esyVo-unsplash.jpg" class="card-img-top d-block w-100" alt="..."/>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="src/temp/car/brandon-atchison-_DzW3MT-iIY-unsplash.jpg" class="card-img-top d-block w-100" alt="..."/>
-                        </div>
+                        ${innerCarousel}
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel-1" data-bs-slide="prev">
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel-${i}" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carousel-1" data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" data-bs-target="#carousel-${i}" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
                 </div>
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                    <h5 class="card-title">${element["title"]}</h5>
                     <p class="card-text">
-                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                        ${element["description"]}
                     </p>
                     <div class="text-center">
                         <a href="src/html/alist.html" class="custom-btn btn-primary">Go somewhere</a>
@@ -35,3 +33,26 @@ export const html = `<div class="col">
                 </div>
             </div>
         </div>`;
+
+    i++;
+  });
+
+  return html;
+}
+
+function createCarousel(mda) {
+  let carousel = "";
+  let j = 0;
+  let active = "active";
+  mda.forEach((element) => {
+    if (j > 0) {
+      active = "";
+    }
+    carousel += `<div class="carousel-item ${active}"> 
+                            <img src="${element}" class="card-img-top d-block w-100" alt="..."/>
+                        </div>`;
+    j++;
+  });
+
+  return carousel;
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,11 @@
-import { feedURL } from "./components/variables";
-console.log(feedURL);
+import { feedContainer, feedURL } from "./components/variables.js";
+import { apiRequest } from "./components/apirequest.js";
+import { createFeedHtml } from "./components/feedHtml.js";
 
-async function feed() {
-  const feedOption = {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  };
-  console.log(feedOption);
+async function loadFeed() {
+  const feedResponse = await apiRequest(feedURL);
+  console.log(feedResponse["json"]);
+  feedContainer.innerHTML = await createFeedHtml(feedResponse["json"]);
 }
 
-feed();
+loadFeed();
