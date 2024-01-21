2ff7419a7c4395ee97a2617eab0b481fbf61bf04
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-01-21 20:30:06 +0100

    removed test redundant test

--- cypress/e2e/view_alist.cy.js
+++ cypress/e2e/view_alist.cy.js
@@ -3,20 +3,6 @@ beforeEach(() => {
 });
 
 describe("Verify view a list link functions", () => {
-  it("opens list page when selected from image", () => {
-    cy.get(".carousel-inner")
-      .first()
-      .find("a")
-      .should("have.attr", "href")
-      .then((href) => {
-        cy.get(".carousel-inner")
-          .first()
-          .click({ force: true })
-          .then(() => {
-            cy.url().should("include", href);
-          });
-      });
-  });
   it("opens list page when selected", () => {
     cy.get(".card-body")
       .first()
