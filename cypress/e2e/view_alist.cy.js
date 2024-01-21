71a46b11dbffe1aa1c67f4819764d703c1dfe84b
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-01-21 18:45:22 +0100

    added view list e2e test

--- cypress/e2e/view_alist.cy.js
+++ cypress/e2e/view_alist.cy.js
@@ -6,18 +6,30 @@ describe("Verify view a list link functions", () => {
   it("opens list page when selected from image", () => {
     cy.get(".carousel-inner")
       .first()
-      .click()
-      .then(() => {
-        cy.url().should("include", "/src/html/alist.html?id=");
+      .find("a")
+      .should("have.attr", "href")
+      .then((href) => {
+        cy.get(".carousel-inner")
+          .first()
+          .click()
+          .then(() => {
+            cy.url().should("include", href);
+          });
       });
   });
   it("opens list page when selected from button", () => {
     cy.get(".card-body")
       .first()
       .find("a")
-      .click()
-      .then(() => {
-        cy.url().should("include", "/src/html/alist.html?id=");
+      .should("have.attr", "href")
+      .then((href2) => {
+        cy.get(".card-body")
+          .first()
+          .find("a")
+          .click()
+          .then(() => {
+            cy.url().should("include", href2);
+          });
       });
   });
 });
